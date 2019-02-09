49f30c740f3255edb7431098251e1ea2e107e093
Radun <raduncicen@gmail.com>
2019-02-09 17:47:36 +0300

    Convert to MVC design pattern ( Add item dediğimde çöküyor. Düzeltilecek)

---
+++ TodoApp/DataModel/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  TodoApp
+//
+//  Created by user on 9.02.2019.
+//  Copyright © 2019 Raduncicen. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
+
+
+
+
